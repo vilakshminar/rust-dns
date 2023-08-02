7efbedf72bd199f012233279f3f111a44b4d366a
Vineeth Lakshminarayanan <vinitlaks@gmail.com>
2023-08-02 09:26:58 -0700

    Modify main to make DNS query to Google's server

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,43 @@ mod dns;
 mod encode;
 mod types;
 
+use anyhow::{Error, Result};
+use std::net::UdpSocket;
 use types::{QClass, QType};
 
 fn main() {
-    run();
+    match run() {
+        Ok(_) => println!("Everything worked!"),
+        Err(e) => eprintln!("An error occurred: {:?}", e),
+    }
 }
 
-fn run() {
-    let qtype = QType::CNAME;
-    let num: u16 = qtype.into();
-    println!("{}", num);
+fn run() -> Result<Vec<u8>, Error> {
+    let query = dns::build_query("www.google.com", QType::A, QClass::IN)?;
 
-    println!(
-        "{:?}",
-        dns::build_query("google.com", QType::CNAME, QClass::IN)
-    );
+    // Create a UDP socket
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+
+    // Query Google's public DNS server.
+    let dns_server = "8.8.8.8:53";
+
+    socket.send_to(&query, dns_server)?;
+
+    /* Buffer to hold received data.
+     * UDP DNS responses are usually less than 512 bytes
+     * See https://www.netmeister.org/blog/dns-size.html for MUCH more on this.
+     * So reading 1024 bytes is enough.
+     */
+    let mut buf = [0u8; 1024];
+
+    // Receive the DNS response.
+    let (_amt, _src) = socket.recv_from(&mut buf)?;
+
+    /*
+     * TODO: Parse the DNS response from buf[..amt].
+     */
+
+    Ok(query)
 }
 
 #[cfg(test)]
@@ -27,8 +49,7 @@ mod test {
 
     #[test]
     fn test_run() -> Result<(), Error> {
-        super::run();
-
+        super::run()?;
         Ok(())
     }
 }
